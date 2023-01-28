9d0a0f77fb40d604cbc7f020c93f20765b848279
Víctor Rodríguez Sanromán <victor.r.sanroman@gmail.com>
2023-01-28 19:48:31 +0100

    Resolve exercise 2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,50 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+//Ejercicio 2
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return $request->all();
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+
+    if ($request->price < 0) {
+        return Response::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    return $request->all();
+});
+
+Route::post('ejercicio2/c', function (Request $request) {
+
+    $code = $request->query('discount');
+    switch ($code) {
+        case 'SAVE5':
+            $discount = 5;
+            break;
+        case 'SAVE10':
+            $discount = 10;
+            break;
+        case 'SAVE15':
+            $discount = 15;
+            break;
+
+        default:
+            $discount = 0;
+            break;
+    }
+
+    $price = $request->price - (($request->price * $discount) / 100);
+    return Response::json(
+        [
+            "name" => $request->name,
+            "description" => $request->description,
+            "price" => $price,
+            "discount" => $discount
+        ]
+    );
+});
+
+
